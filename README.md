22396475633c2c8d835ac05f5c678b077921caf8
John McManus <john@appyappster.com>
2021-10-21 21:16:30 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # kubeexec
+# kubeexec
---
+++ kubeexec/kubeexecApp.swift
@@ -0,0 +1,17 @@
+//
+//  kubeexecApp.swift
+//  kubeexec
+//
+//  Created by John McManus on 21/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct kubeexecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
